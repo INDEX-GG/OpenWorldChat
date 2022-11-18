5d1a03c7a098068ae17c14a3832db1bf54897fcc
NenadoPL1Z <limon4ikpro1@gmail.com>
2022-11-18 17:21:03 +0500

    forward push

--- server/app.js
+++ server/app.js
@@ -1,10 +1,1 @@
-const app = require("express")();
-const httpServer = require("http").createServer();
-const io = require("socket.io")(httpServer, {
-    path: "/",
-    cors: {origin: "*"}
-});
-
-io.on("connection", socket => { console.log('a user connected'); });
-
-httpServer.listen(8000);
+console.log(123);
