c8e321e75880e9319474f1ffffc4242ea18f5e34
Gosha Kokorin <go_2002@mail.ru>
2022-12-09 15:10:21 +0500

    change: IS_DEV

--- front/src/lib/constants/constants.ts
+++ front/src/lib/constants/constants.ts
@@ -5,12 +5,10 @@ export const ADMIN_ID = +(process.env.REACT_APP_ADMIN_ID || 0);
 export const SECRET_KEY = process.env.REACT_APP_SECRET_KEY;
 export const ADMIN_ALL_ROOM_NAME = process.env.REACT_APP_ADMIN_ALL_ROOM_NAME;
 
-export const BASE_URL = IS_DEV
-  ? "http://192.168.88.83:4000"
-  : process.env.REACT_APP_BASE_URL;
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-export const SOCKET_PATH = IS_PROD ? "/socket.io/" : "";
-export const PATH_URL = IS_DEV ? "" : process.env.REACT_APP_PATH_URL || "";
+export const SOCKET_PATH = "/socket.io/";
+export const PATH_URL = "";
 export const API_URL = BASE_URL + PATH_URL;
 export const PATH_URL_FRONTEND = process.env.REACT_APP_PATH_URL_FRONTEND;
 export const SOCKET_PATH_URL = `${PATH_URL}${SOCKET_PATH}`;
