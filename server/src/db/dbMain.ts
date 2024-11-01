8057c82c29ac51ebc3263b0b5681082e626adb97
NenadoPL1Z <limon4ikpro1@gmail.com>
2024-11-02 02:06:35 +0500

    fix: remove DB_NAME_MAIN

--- server/src/db/dbMain.ts
+++ server/src/db/dbMain.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize"
-import {DB_HOST, DB_NAME_MAIN, DB_PASS, DB_PORT, DB_USER} from "../constants/constants";
+import {DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER} from "../constants/constants";
 
-export default new Sequelize(`postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME_MAIN}`, { logging: false, dialectOptions: {
+export default new Sequelize(`postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, { logging: false, dialectOptions: {
     statement_timeout: 1000,
 }})
